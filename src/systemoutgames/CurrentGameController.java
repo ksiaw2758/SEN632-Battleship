1e65c5eb48a73b1814fa56d56c6199e7c292336b
Zach Schulz <schulzzw@gmail.com>
2017-01-25 22:25:21 -0700

    Ask AI player for ships and place them on the secondary grid

--- src/systemoutgames/CurrentGameController.java
+++ src/systemoutgames/CurrentGameController.java
@@ -9,7 +9,6 @@ import main.Main;
 
 import java.io.IOException;
 import java.net.URL;
-import java.util.ArrayList;
 import java.util.List;
 
 public class CurrentGameController {
@@ -18,32 +17,7 @@ public class CurrentGameController {
     private Player playerTwo;
 
     private Grid primaryGrid;
-
-    @FXML
-    private String aircraftCarrierLocation;
-    @FXML
-    private String aircraftCarrierDirection;
-
-    @FXML
-    private String battleShipLocation;
-    @FXML
-    private String battleShipDirection;
-
-    @FXML
-    private String submarineLocation;
-    @FXML
-    private String submarineDirection;
-
-    @FXML
-    private String crusierLocation;
-    @FXML
-    private String crusierDirection;
-
-    @FXML
-    private String destroyerLocation;
-    @FXML
-    private String destroyerDirection;
-
+    private Grid secondaryGrid;
 
 
     public CurrentGameController(Player playerOne, Player playerTwo) {
@@ -59,9 +33,10 @@ public class CurrentGameController {
 
         FXMLLoader loader = new FXMLLoader(gameScreen);
 
-        List<Ship> ships = createShips();
+        List<Ship> playerOneShips = playerOne.selectShips();
+        List<Ship> playerTwoShips = playerTwo.selectShips();
 
-        createGrids(ships);
+        createGrids(playerOneShips, playerTwoShips);
 
         loader.setController(this);
 
@@ -76,59 +51,12 @@ public class CurrentGameController {
         border.setCenter(paneGame);
     }
 
-    private List<Ship> createShips() {
-        List<Ship> ships = new ArrayList<>();
-
-        ships.add(new Ship(
-                "Aircarft Carrier",
-                5,
-                new Location(aircraftCarrierLocation.charAt(0), aircraftCarrierLocation.charAt(0), getDirection(aircraftCarrierDirection))
-        ));
-
-        ships.add(new Ship(
-                "Battle Ship",
-                4,
-                new Location(battleShipLocation.charAt(0), battleShipLocation.charAt(0), getDirection(battleShipDirection))
-        ));
-
-        ships.add(new Ship(
-                "Submarine",
-                3,
-                new Location(submarineLocation.charAt(0), submarineLocation.charAt(0), getDirection(submarineDirection))
-        ));
-
-        ships.add(new Ship(
-                "Crusier",
-                3,
-                new Location(crusierLocation.charAt(0), crusierLocation.charAt(0), getDirection(crusierDirection))
-        ));
-
-        ships.add(new Ship(
-                "Destroyer",
-                3,
-                new Location(destroyerLocation.charAt(0), destroyerLocation.charAt(0), getDirection(destroyerDirection))
-        ));
-
-        return ships;
-    }
-
-    private Direction getDirection(String shipDirection) {
-        String direction = shipDirection.toLowerCase();
-
-        switch(direction){
-            case "horizontal":
-                return Direction.HORIZONTAL;
-            case "vertical":
-                return Direction.VERTICLE;
-            default:
-                return Direction.HORIZONTAL;
-        }
-    }
-
-    private void createGrids(List<Ship> ships) {
+    private void createGrids(List<Ship> playerOneShips, List<Ship> playerTwoShips) {
         primaryGrid = new Grid();
+        secondaryGrid = new Grid();
 
-        primaryGrid.placeShips(ships);
+        primaryGrid.placeShips(playerOneShips);
+        secondaryGrid.placeShips(playerTwoShips);
     }
 
 
